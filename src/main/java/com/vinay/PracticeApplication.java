f9a91cb228ad301f4f93829b900a76896403937f
Vinay Kumbar <vinaykumbar01@gmail.com>
2025-03-15 20:16:31 +0530

    2nd commit

--- src/main/java/com/vinay/PracticeApplication.java
+++ src/main/java/com/vinay/PracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeApplication {
 
 	public static void main(String[] args) {
+int x = 100;
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
